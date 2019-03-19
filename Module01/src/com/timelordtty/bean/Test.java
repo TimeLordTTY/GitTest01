8832e0f9d32bbb59af17fc9a3c71fba3868387b2
TimeLordTTY <987334289@qq.com>
2019-03-19 19:21:48 +0800

    测试2

--- Module01/src/com/timelordtty/bean/Test.java
+++
@@ -1,8 +0,0 @@
-package com.timelordtty.bean;
-
-/**
- * @author TimeLordTTY
- * @createtime 2019-03-19 19:16
- */
-public class Test {
-}
--- src/com/timelordtty/gittest/Test.java
+++
@@ -1,8 +0,0 @@
-package com.timelordtty.gittest;
-
-/**
- * @author TimeLordTTY
- * @createtime 2019-03-19 19:02
- */
-public class Test {
-}
